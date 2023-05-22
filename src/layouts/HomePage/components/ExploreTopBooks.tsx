aa205775867e546bcb08ecaeae35c94a95a49f84
unknown <yeatz0408@gmail.com>
2023-05-22 18:44:29 +0900

    change ExploreTopBook string

--- src/layouts/HomePage/components/ExploreTopBooks.tsx
+++ src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -8,7 +8,7 @@ export const ExploreTopBooks = () => {
                     <h1 className='display-5 fw-bold'>次はどんなトレーニングの本を?</h1>
                     <p className='col-md-8 fs-4'>より機能的な体のために勉強しましょう。</p>
                     <Link type='button' className='btn main-color btn-lg text-white' to='/search'>
-                        一番人気の本を探す
+                        新しい本を探す
                     </Link>
                 </div>
             </div>
