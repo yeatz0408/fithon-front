0cae312e2cb5704ae67afda2ca629a2896e30ae3
unknown <yeatz0408@gmail.com>
2023-05-23 04:17:13 +0900

    add test user log in information alert when not logged in at Home

--- src/layouts/HomePage/HomePage.tsx
+++ src/layouts/HomePage/HomePage.tsx
@@ -1,9 +1,20 @@
+import { useOktaAuth } from "@okta/okta-react"
 import { Carousel } from "./components/Carousel"
 import { ExploreTopBooks } from "./components/ExploreTopBooks"
 import { Heroes } from "./components/Heroes"
 import { LibraryServices } from "./components/LibraryServices"
 
 export const HomePage = () => {
+
+    const { authState } = useOktaAuth();
+
+    if (authState && authState.isAuthenticated === false) {
+        alert("是非ログインして全ての機能を確認してください！" +
+            "\n\nテストID\n" +
+            "ID: testuser5@email.com \n" +
+            "PW: testtest11");    
+    }
+
     return (
         <div>
             <ExploreTopBooks />
